1497baa51f778345eacbfb08408e6fccd7032e12
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-06-24 21:54:15 +0000

    modificado y creado el archivo js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import "../src/style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateRandomNumber = () => {
+  const card = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let position = Math.floor(Math.random() * card.length);
+  return card[position];
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomSuit = () => {
+  const pinta = ["♦", "♥", "♠", "♣"];
+  let indexPinta = Math.floor(Math.random() * pinta.length);
+  return pinta[indexPinta];
+};
+let randomSuit = generateRandomSuit();
+let randomNumber = generateRandomNumber();
+
+window.onload = () => {
+  let topSuit = document.querySelector(".top-suit");
+  let number = document.querySelector(".number");
+  let bottomSuit = document.querySelector(".bottom-suit");
+
+  topSuit.innerHTML = randomSuit;
+  number.innerHTML = randomNumber;
+  bottomSuit.innerHTML = randomSuit;
+
+  if (randomSuit == "♦" || randomSuit == "♥") {
+    topSuit.style.color = "red";
+    bottomSuit.style.color = "red";
+  }
 };
